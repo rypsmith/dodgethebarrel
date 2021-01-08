5f4b8fa601a242fa918733defe152ffc3ca5dfcd
rypsmith <rypsmith@users.noreply.github.com>
2021-01-07 22:59:21 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,140 @@
+enum SpriteKindLegacy {
+    Player,
+    Projectile,
+    Food,
+    Enemy
+}
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (mySprite.isHittingTile(CollisionDirection.Bottom)) {
+        mySprite.vy = -250
+    }
+})
+scene.onHitTile(SpriteKindLegacy.Projectile, 9, function (sprite) {
+    info.changeScoreBy(1)
+})
+sprites.onOverlap(SpriteKindLegacy.Player, SpriteKindLegacy.Projectile, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    if (info.life() > 0) {
+        info.changeLifeBy(-1)
+    } else {
+        game.over(false, effects.confetti)
+    }
+})
+sprites.onOverlap(SpriteKindLegacy.Player, SpriteKindLegacy.Food, function (sprite, otherSprite) {
+    otherSprite.destroy()
+    info.changeLifeBy(1)
+})
+scene.onHitTile(SpriteKindLegacy.Food, 9, function (sprite) {
+    sprite.destroy()
+})
+let projectile: Sprite = null
+let myTile: tiles.Tile = null
+let projectile2: Sprite = null
+let randNum = 0
+let mySprite: Sprite = null
+scene.setTile(7, img`
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 7 
+    `, true)
+scene.setTile(9, img`
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 
+    `, true)
+scene.setTileMap(img`
+    9 . . . . . . . . . 
+    9 . . . . . . . . . 
+    9 . . . . . . . . . 
+    9 . . . . . . . . . 
+    9 . . . . . . . . . 
+    9 . . . . . . . . . 
+    7 7 7 7 7 7 7 7 7 7 
+    2 2 2 2 2 2 2 2 2 2 
+    `)
+mySprite = sprites.create(img`
+    . . . . . . 5 . 5 . . . . . . . 
+    . . . . . f 5 5 5 f f . . . . . 
+    . . . . f 1 5 2 5 1 6 f . . . . 
+    . . . f 1 6 6 6 6 6 1 6 f . . . 
+    . . . f 6 6 f f f f 6 1 f . . . 
+    . . . f 6 f f d d f f 6 f . . . 
+    . . f 6 f d f d d f d f 6 f . . 
+    . . f 6 f d 3 d d 3 d f 6 f . . 
+    . . f 6 6 f d d d d f 6 6 f . . 
+    . f 6 6 f 3 f f f f 3 f 6 6 f . 
+    . . f f d 3 5 3 3 5 3 d f f . . 
+    . . f d d f 3 5 5 3 f d d f . . 
+    . . . f f 3 3 3 3 3 3 f f . . . 
+    . . . f 3 3 5 3 3 5 3 3 f . . . 
+    . . . f f f f f f f f f f . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKindLegacy.Player)
+mySprite.setPosition(40, 70)
+mySprite.ay = 500
+info.setLife(3)
+game.onUpdateInterval(randint(900, 1600), function () {
+    randNum = randint(1, 7)
+    if (randNum % 7 == 0) {
+        projectile2 = sprites.create(img`
+            . . . . . . . . . . . 6 6 6 6 6 
+            . . . . . . . . . 6 6 7 7 7 7 8 
+            . . . . . . 8 8 8 7 7 8 8 6 8 8 
+            . . e e e e c 6 6 8 8 . 8 7 8 . 
+            . e 2 5 4 2 e c 8 . . . 6 7 8 . 
+            e 2 4 2 2 2 2 2 c . . . 6 7 8 . 
+            e 2 2 2 2 2 2 2 c . . . 8 6 8 . 
+            e 2 e e 2 2 2 2 e e e e c 6 8 . 
+            c 2 e e 2 2 2 2 e 2 5 4 2 c 8 . 
+            . c 2 e e e 2 e 2 4 2 2 2 2 c . 
+            . . c 2 2 2 e e 2 2 2 2 2 2 2 e 
+            . . . e c c e c 2 2 2 2 2 2 2 e 
+            . . . . . . . c 2 e e 2 2 e 2 c 
+            . . . . . . . c e e e e e e 2 c 
+            . . . . . . . . c e 2 2 2 2 c . 
+            . . . . . . . . . c c c c c . . 
+            `, SpriteKindLegacy.Food)
+        projectile2.vx = randint(-100, -60)
+        myTile = scene.getTile(9, 3)
+        myTile.place(projectile2)
+    } else {
+        projectile = sprites.createProjectileFromSide(img`
+            . e e e e e e . 
+            e e e e e e e e 
+            1 1 1 1 1 1 1 1 
+            e e e e e e e e 
+            e e e e e e e e 
+            1 1 1 1 1 1 1 1 
+            e e e e e e e e 
+            . e e e e e e . 
+            `, randint(-100, -60), 0)
+        myTile = scene.getTile(9, 5)
+        myTile.place(projectile)
+    }
+})
